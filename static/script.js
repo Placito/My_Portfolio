3dccc5adf7d49c5aa8d627ccabc643210132acba
Mariana <101410421+Placito@users.noreply.github.com>
2024-05-10 17:59:00 +0000

    add skills section

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-/* Abre e fecha menu lateral em modo mobile */
+/* Open and close side menu in mobile mode */
 const menuMobile = document.querySelector('.menu-mobile');
 const body = document.querySelector('body');
 
@@ -8,3 +8,15 @@ menuMobile.addEventListener('click', () => {
         : menuMobile.classList.replace('bi-x', 'bi-list');
     body.classList.toggle('menu-nav-active');
 });
+
+/* closes the menu when clicking on an item in responsive mode */
+const navItem = document.querySelectorAll('.nav-item');
+
+navItem.forEach(item => {
+    item.addEventListener('click', () => {
+        if (body.classList.contains('menu-nav-active')) {
+            body.classList.remove('menu-nav-active')
+            menuMobile.classList.replace('bi-x', 'bi-list');
+        }
+    })
+})
