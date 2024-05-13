57e16f1e08f7666a2ef3c7763ab779f6a672de1c
Mariana <101410421+Placito@users.noreply.github.com>
2024-05-13 16:16:31 +0000

    add animations

--- static/script.js
+++ static/script.js
@@ -20,3 +20,25 @@ navItem.forEach(item => {
         }
     })
 })
+
+// animate all itens with data-anime atribute
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.scrollY + window.innerHeight * 0.85;
+    console.log(windowTop)
+
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate");
+        }
+    }); 
+};
+
+animeScroll();
+
+window.addEventListener("scroll", () => {
+    animeScroll();
+})
