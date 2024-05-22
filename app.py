db68ded1b10bfd89eaa8de4d4747f0a331147cc8
Mariana <101410421+Placito@users.noreply.github.com>
2024-05-22 13:38:41 +0100

    add port

--- app.py
+++ app.py
@@ -152,4 +152,4 @@ def download_file(filename):
 # Serve the application with Waitress
 if __name__ == '__main__':
     app.run()
-    #serve(app, host='0.0.0.0', port=8080)
+    serve(app, host='0.0.0.0', port=8080)
