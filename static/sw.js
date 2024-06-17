fe207aeb79d972ba5e61a2d3f99c1bf17bbf9bd2
Mariana <101410421+Placito@users.noreply.github.com>
2024-06-17 11:07:05 +0000

    subscribe function called

--- static/sw.js
+++ static/sw.js
@@ -4,10 +4,10 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('my-cache').then(function(cache) {
             return cache.addAll([
-                '../templates/index.html',
-                './static/style.min.css',
-                './static/script.js',
-                './static/sw.js'
+                '/templates/index.html',
+                '/static/style.min.css',
+                '/static/script.js',
+                '/static/sw.js'
             ]).catch(function(error) {
                 console.error('Failed to add resource to cache:', error);
             });
